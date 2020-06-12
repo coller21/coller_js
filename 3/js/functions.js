bc5c290d57d7220ef27a2f6ca1829530658a8a8e
John <coller21@yandex.ru>
2020-06-12 14:02:34 +0300

    lesson3 fixbags

--- 3/js/functions.js
+++ 3/js/functions.js
@@ -1,29 +1,29 @@
 function randomInteger(min, max) {
-    let rand = min + Math.random() * (max + 1 - min);
-    return Math.floor(rand);
-  }
+  let rand = min + Math.random() * (max + 1 - min);
+  return Math.floor(rand);
+}
 
-  let goBot = (num) => {
-    if (num === null) {
-        alert('Game Over');
+let goBot = (num) => {
+  if (num === null) {
+    alert('Game Over');
+  } else {
+    if (Number(num).toString() != 'NaN' && num != '') {
+      numUser = Number(num);
+      if (numUser > randomNumber) {
+        alert('Меньше');
+        let numberUser = prompt('Введите число');
+        setTimeout(goBot(numberUser), 0);
+      } else if (numUser < randomNumber) {
+        alert('Больше');
+        let numberUser = prompt('Введите число');
+        setTimeout(goBot(numberUser), 0);
+      } else {
+        alert('Правильно');
+      }
     } else {
-        if (Number(num).toString() != 'NaN' && num != '') {
-            numUser = Number(num);
-            if (numUser > randomNumber) {
-                alert('Меньше');
-                let numberUser = prompt('Введите число');
-                goBot(numberUser);
-            } else if (numUser < randomNumber) {
-                alert('Больше');
-                let numberUser = prompt('Введите число');
-                goBot(numberUser);
-            } else {
-                alert('Правильно');
-            }
-        } else {
-            alert('Введите число')
-            let numberUser = prompt('Введите число');
-            goBot(numberUser);
-        }
+      alert('Введите число')
+      let numberUser = prompt('Введите число');
+      setTimeout(goBot(numberUser), 0);
     }
+  }
 }
