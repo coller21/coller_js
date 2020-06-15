1f3079f7d4f0ec6878441c91bd922b6801b52965
unknown <C:\Users\John\AppData\Roaming\The Bat!>
2020-06-15 18:02:13 +0300

    start 5 lesson

---
+++ 5/js/lib.js
@@ -0,0 +1,37 @@
+// function randomInteger(min, max) {
+//   let rand = min + Math.random() * (max + 1 - min);
+//   return Math.floor(rand);
+// }
+
+// let goBot = (num) => {
+//   if (num === null) {
+//     alert('Game Over');
+//   } else {
+//     if (Number(num).toString() != 'NaN' && num != '') {
+//       numUser = Number(num);
+//       if (numUser > randomNumber) {
+//         alert('Меньше');
+//         let numberUser = prompt('Введите число');
+//         setTimeout(goBot(numberUser), 0);
+//       } else if (numUser < randomNumber) {
+//         alert('Больше');
+//         let numberUser = prompt('Введите число');
+//         setTimeout(goBot(numberUser), 0);
+//       } else {
+//         alert('Правильно');
+//       }
+//     } else {
+//       alert('Введите число')
+//       let numberUser = prompt('Введите число');
+//       setTimeout(goBot(numberUser), 0);
+//     }
+//   }
+// }
+
+(function () {
+  let startGame = confirm('Вы хотите начать игру?');
+
+  window.start = function () {
+    return startGame;
+  }
+})()
--- 5/js/script.js
+++
@@ -1,5 +0,0 @@
-let randomNumber = randomInteger(1, 10);
-
-let numberUser = prompt('Введите число').replace(/ /g, "");
-
-goBot(numberUser);
