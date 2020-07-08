ea9bf0116b31a3e5aefbe11bc2f6f57e41702aa8
unknown <C:\Users\John\AppData\Roaming\The Bat!>
2020-07-08 17:47:59 +0300

    try 3

--- 5/js/lib.js
+++ 5/js/lib.js
@@ -1,42 +1,39 @@
 (function () {
-let startGame = confirm('Вы хотите начать игру?');
-let randomNumber = Math.round(Math.random() * 1000);
-let counterTry = 0;
-let goBot = (num) => {
-  if (num === null) {
-    alert('Game Over');
-  } else {
-    if (counterTry < 10) {
-      if (Number(num).toString() != 'NaN' && num != '') {
-        numUser = Number(num);
-        if (numUser > randomNumber) {
-          counterTry++;
-          alert('Меньше');
-          let numberUser = prompt('Введите число');
-          goBot(numberUser);
-        } else if (numUser < randomNumber) {
-          counterTry++;
-          alert('Больше');
-          let numberUser = prompt('Введите число');
-          goBot(numberUser);
+  function startGame() {
+    let startGame = confirm('Вы хотите начать игру?');
+    return [randomNumber = Math.round(Math.random() * 1000), counterTry = 0];
+  }
+  startGame();
+  window.start = function (num) {
+    if (num === null) {
+      alert('Game Over');
+    } else {
+      if (counterTry < 10) {
+        if (Number(num).toString() != 'NaN' && num != '') {
+          numUser = Number(num);
+          if (numUser > randomNumber) {
+            counterTry++;
+            alert('Меньше');
+            let numberUser = prompt('Введите число');
+            window.start(numberUser);
+          } else if (numUser < randomNumber) {
+            counterTry++;
+            alert('Больше');
+            let numberUser = prompt('Введите число');
+            window.start(numberUser);
+          } else {
+            alert('Правильно');
+          }
         } else {
-          alert('Правильно');
+          let numberUser = prompt('Введите число');
+          window.start(numberUser);
         }
       } else {
-        let numberUser = prompt('Введите число');
-        goBot(numberUser);
+        alert('Количество попыток превышено');
+        startGame = confirm('Вы хотите начать игру?');
+        counterTry = 0;
+        window.start();
       }
-    } else {
-      alert('Количество попыток превышено');
-      startGame = confirm('Вы хотите начать игру?');
-      counterTry = 0;
-      goBot();
     }
-  }
-}
-
-  window.start = function () {
-    goBot();
-    return startGame;
   }
 })()
