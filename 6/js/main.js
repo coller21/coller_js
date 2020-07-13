eef28933205a7243d60fbfc1d88f325e72946dad
unknown <C:\Users\John\AppData\Roaming\The Bat!>
2020-07-13 17:00:09 +0300

    Lesson 6 finished

--- 6/js/main.js
+++ 6/js/main.js
@@ -1,41 +1,41 @@
-(function () {
-  let randomNumber = 0;
-  let counterTry = 0;
-  function startGame() {
-    let start = confirm('Вы хотите начать игру?');
-    randomNumber = Math.round(Math.random() * 1000);
-    counterTry = 0;
+function UserList() {
+  this.users = [];
+
+  this.add = function (user) {
+    (this.users).push(user);
   }
-  startGame();
-  window.start = function (num) {
-    if (num === null) {
-      alert('Game Over');
-    } else {
-      if (counterTry < 10) {
-        if (Number(num).toString() != 'NaN' && num != '') {
-          numUser = Number(num);
-          if (numUser > randomNumber) {
-            counterTry++;
-            alert('Меньше');
-            let numberUser = prompt('Введите число');
-            window.start(numberUser);
-          } else if (numUser < randomNumber) {
-            counterTry++;
-            alert('Больше');
-            let numberUser = prompt('Введите число');
-            window.start(numberUser);
-          } else {
-            alert('Правильно');
-          }
-        } else {
-          let numberUser = prompt('Введите число');
-          window.start(numberUser);
-        }
-      } else {
-        alert('Количество попыток превышено');
-        startGame();
-        window.start();
-      }
+
+  this.getAllUsers = function () {
+    for ( i=0; i <= this.users.length; i++) {
+     console.log(this.users[i]);
     }
   }
-})()
+}
+
+function User(firstName, lastName) {
+  this.firstName = firstName;
+  this.lastName = lastName;
+  let myDate = new Date();
+  this.regDate = ('0' + myDate.getDate()).slice(-2) + '-'
+    + ('0' + (myDate.getMonth() + 1)).slice(-2) + '-'
+    + myDate.getFullYear() + " | "
+    + ('0' + myDate.getHours()).slice(-2) + ':'
+    + ('0' + myDate.getMinutes()).slice(-2);
+
+}
+
+let emptyUserList = new UserList();
+let dialogWindow = prompt('Введите имя и фамилию');
+
+while (dialogWindow !== null) {
+  if (dialogWindow == '') {
+    alert('Вы ничего не ввели');
+    dialogWindow = prompt('Введите имя и фамилию');
+  }
+  let splitString = dialogWindow.split(' ', 2);
+  let newUser = new User(splitString[0], splitString[1]);
+  emptyUserList.add(newUser);
+  dialogWindow = prompt('Введите имя и фамилию');
+}
+
+emptyUserList.getAllUsers();
