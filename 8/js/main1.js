22701be3a45a2591b6533ce8788abfd0525ee82a
unknown <C:\Users\John\AppData\Roaming\The Bat!>
2020-07-22 11:14:54 +0300

    finish lesson 8

--- 8/js/main1.js
+++ 8/js/main1.js
@@ -1,11 +1,29 @@
-let userArray = ['string', 10, 20, 'a', 'b', true, false];
-function filterByType () {
-  let userArgs = Array.prototype.slice.call(arguments);
-
-  for (let elem of userArgs) {
-    console.log(typeof(elem));
+// По Китайски с arguments
+function filterByType() {
+  let [type, ...elements] = [...arguments];
+  let newArray = [];
+  for (let elem of elements) {
+    if (type === typeof (elem))
+      newArray.push(elem);
   }
-
+  return newArray;
 }
 
-filterByType(userArray);
+console.log(filterByType('string', 10, 20, 'a', 'b', true, false, 'привет', 'аргументы'));
+
+// Вариант без arguments
+function filterByType1(type, ...elements) {
+  let newArray1 = [];
+  for (let elem of elements) {
+    if (type === typeof (elem))
+      newArray1.push(elem);
+  }
+  return newArray1;
+}
+
+console.log(filterByType1('number', 10, 20, 'a', 'b', true, false, 123));
+
+// Минутка функциональной наркомании ))
+
+let filterByType2 = (type, ...args) => args.filter(arg => typeof (arg) === type);
+console.log(filterByType2('boolean', 10, 20, 'a', 'b', true, false, true, true, false));
