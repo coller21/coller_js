be218e17f6cab5e14068c4a3b4fc191df1c9d5bc
unknown <C:\Users\John\AppData\Roaming\The Bat!>
2020-07-29 15:42:14 +0300

    aadda

--- 9.1/js/main.js
+++ 9.1/js/main.js
@@ -1,53 +1,37 @@
 
-let inputFocus = document.querySelector('#input');
-inputFocus.focus();
-
+const inputFocus = document.querySelector('#input');
+const listOl = document.querySelector('.task-list');
 
 let submitEvent = document.querySelector('#form');
+
 submitEvent.addEventListener('submit', function (ev) {
   ev.preventDefault();
-  let textInput = document.querySelector('#input').value;
+  let textInput = inputFocus.value.trim();
   if (textInput != '') {
     let newLi = document.createElement('li');
     newLi.classList.add('inserted-list-item');
-    newLi.innerHTML = textInput;
-
-    let inputPlace = document.querySelector('#input_field');
-    let parrentPr = inputPlace.parentNode;
-    parrentPr.insertBefore(newLi, inputPlace);
-    document.querySelector('#input').value = '';
-    inputFocus.focus();
+    newLi.setAttribute('tabindex', 2);
+    newLi.textContent = textInput;
+    listOl.append(newLi);
+    inputFocus.value = '';
+    inputFocus.classList.remove('border-danger');
+    newLi.addEventListener('keydown', function (ev) {
+      if (ev.keyCode == 13 || ev.keyCode == 32) {
+        if (!ev.target.classList.contains('inserted-list-item')) return;
+        ev.target.classList.toggle('line-text');
+      }
+    })
   } else {
     alert('Вы не ввели задачу!');
+    inputFocus.classList.add('border-danger');
+    inputFocus.focus();
   }
 });
 
-document
-  .querySelector('.task-list')
-  .addEventListener('click', function (ev) {
-    if (!ev.target.classList.contains('inserted-list-item')) return;
-    ev.target.classList.toggle('line-text');
-    inputFocus.focus();
-  });
+listOl.addEventListener('click', function (ev) {
+  if (!ev.target.classList.contains('inserted-list-item')) return;
+  ev.target.classList.toggle('line-text');
+});
 
-document
-  .querySelector('.task-list')
-  .addEventListener('dblclick', function (ev) {
-    if (!ev.target.classList.contains('inserted-list-item')) return;
-    let textItem = ev.target.textContent;
-    let newInput = document.createElement('input');
-    newInput.classList.add('form-control');
-    newInput.classList.add('edit-field');
-    newInput.setAttribute('value', textItem);
-    newInput.setAttribute('type', 'text');
-    ev.target.innerHTML = '';
-    ev.target.append(newInput);
-    newInput.focus();
-    document.querySelector('.edit-field').addEventListener('change', function (ev) {
-      let parrentPr1 = ev.target.parentNode;
-      parrentPr1.innerHTML = ev.target.value;
-      if (ev.target.value == '') parrentPr1.remove();
-      inputFocus.focus();
-    });
-  });
+
 
