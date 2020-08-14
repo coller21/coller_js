f6df7e80b5a327cdb874b2b2002176a50735eefd
unknown <C:\Users\John\AppData\Roaming\The Bat!>
2020-08-14 18:01:30 +0300

    finish19-pro

--- 10/lesson10/js/main.js
+++ 10/lesson10/js/main.js
@@ -2,27 +2,54 @@ const field = document.querySelector('.text');
 const editField = document.querySelector('.edit');
 const saveField = document.querySelector('.save');
 const cancelField = document.querySelector('.cancel');
+const selectList = document.querySelector('.change-list');
+const textField = JSON.parse(localStorage.getItem('text'));
+let arrayToLocal = [];
 
-const textField = localStorage.getItem('text');
-
-if(!textField) {
-  field.innerHTML = 'Нажмите кнопку "редактировать".';
-} else {
-  field.innerHTML = textField;
+let doSelect = () => {
+  while (selectList.firstChild) {
+    selectList.firstChild.remove();
+  }
+  for (elem in textField) {
+    let newOption = new Option(new Date(textField[elem].date).toLocaleDateString() + ' | ' +
+    new Date(textField[elem].date).toLocaleTimeString() + ' - ' + textField[elem].text, textField[elem].text);
+    selectList.append(newOption);
+  }
 }
 
-editField.addEventListener('click', function(){
+selectList.addEventListener('change', function (ev) {
+  field.innerHTML = ev.target.value;
+})
+
+if (!textField) {
+  field.innerHTML = 'Нажмите кнопку "редактировать".';
+} else {
+  field.innerHTML = textField[textField.length - 1].text;
+  arrayToLocal = textField;
+  doSelect();
+}
+
+let doArray = (nowDate, insertedText) => {
+  let objectData = {};
+  objectData.date = nowDate;
+  objectData.text = insertedText;
+  arrayToLocal.push(objectData);
+  localStorage.setItem('text', JSON.stringify(arrayToLocal));
+}
+
+editField.addEventListener('click', function () {
   field.toggleAttribute('contenteditable');
   document.querySelectorAll('.btn-grupe .check').forEach(element => element.toggleAttribute('disabled'));
 })
 
-saveField.addEventListener('click', function(){
+saveField.addEventListener('click', function () {
   document.querySelectorAll('.btn-grupe .check').forEach(element => element.toggleAttribute('disabled'));
   field.toggleAttribute('contenteditable');
-  localStorage.setItem('text', field.innerHTML);
+  doArray(new Date(), field.innerHTML)
+  doSelect();
 })
 
-cancelField.addEventListener('click', function(){
+cancelField.addEventListener('click', function () {
   document.querySelectorAll('.btn-grupe .check').forEach(element => element.toggleAttribute('disabled'));
   field.toggleAttribute('contenteditable');
   field.innerHTML = localStorage.getItem('text');
